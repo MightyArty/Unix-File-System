2a12e6e1f4d73c7c7033763494f1b5ab455bccec
Artem Shabalin <tomshabalin95@gmail.com>
2022-06-05 14:06:26 +0300

    1 progress

--- ufs.cpp
+++ ufs.cpp
@@ -3,63 +3,77 @@
 /**
  * Global variables
  */
-struct superblock super;
-struct block *block_arr;
+s_block super;
+block *block_arr;
 struct inode *inode_arr;
+struct myopenfile opened[MAX_FILES];
 
-void create_fs(){
-    super.blocks_amount = 50;
-    super.inodes_amount = 10;
-    super.blokcs_size = sizeof(struct block);
+void mymkfs(int s){
+    int update_size = s - (sizeof(s_block));
+    int free_space = (update_size/10);
+    super.inodes_amount = (free_space)/(sizeof(i_node));
+    super.blocks_amount = (update_size - free_space)/(sizeof(block));
+    super.blokcs_size = sizeof(block);
     
-    inode_arr = (struct inode *)malloc(sizeof(struct inode) * super.inodes_amount);
+    inode_arr = (i_node *)malloc(sizeof(i_node) * super.inodes_amount);
     for(int i = 0 ; i < super.inodes_amount ; i++){
         inode_arr[i].size = -1; // if not allocated
         inode_arr[i].first_block = -1;
         strcpy(inode_arr[i].id, "empty"); // initialize id to nothing
     }
-    block_arr = (struct block *)malloc(sizeof(struct block) * super.blocks_amount);
+
+    block_arr = (block *)malloc(sizeof(block) * super.blocks_amount);
     for(int i = 0 ; i < super.blocks_amount ; i++){
         block_arr[i].next_block = -1;
+        strcpy(block_arr[i].data, "empty"); // initialize data to nothing
     }
 }
 
-void mount_fs(){
+void sync_fs(const char *ch){
     FILE *file;
-    file = fopen("data", "r");  //reat
+    file = fopen(ch, "w+");
 
     // superblock
-    fread(&super, sizeof(super), 1, file);
+    fwrite(&super, sizeof(s_block), 1, file);
 
     // inodes
     for(int i = 0 ; i < super.inodes_amount ; i++){
-        fread(&(inode_arr[i]), sizeof(struct inode), 1, file);
-    }
-
-    // blokcs
-    for(int i = 0 ; i < super.blocks_amount ; i++){
-        fread(&(block_arr[i]), sizeof(struct block), 1, file);
-    }
-}
-
-void sync_fs(){
-    FILE *file;
-    file = fopen("data", "w+"); //write
-
-    // superblock
-    fwrite(&super, sizeof(super), 1, file);
-
-    // inodes
-    for(int i = 0 ; i < super.inodes_amount ; i++){
-        fwrite(&(inode_arr[i]), sizeof(struct inode), 1, file);
+        fwrite(&(inode_arr[i]), sizeof(i_node), 1, file);
     }
 
     //blocks
     for(int i = 0 ; i < super.blocks_amount ; i++){
-        fwrite(&(block_arr[i]), sizeof(struct block), 1, file);
+        fwrite(&(block_arr[i]), sizeof(block), 1, file);
     }
 
     fclose(file);
+}
+
+void mount_fs(const char *ch){
+    FILE *file;
+    file = fopen(ch, "r"); 
+
+    // superblock
+    fread(&super, sizeof(struct superblock), 1, file);
+
+    inode_arr = (struct inode *)malloc(sizeof(struct inode) * super.inodes_amount);
+    block_arr = (struct block *)malloc(sizeof(struct block) * super.blocks_amount);
+
+    fread(inode_arr, sizeof(struct inode), super.inodes_amount, file);    
+    fread(block_arr, sizeof(struct block), super.blocks_amount, file);
+
+    fclose(file);
+}
+
+int mymount(const char *source, const char *target, const char *filesystemtype, unsigned long mountflags, const void *data){
+    // write to target
+    if(target != NULL)
+        sync_fs(target);
+    // read the source
+    else if(source != NULL)
+        mount_fs(source);
+    
+    return 0;
 }
 
 void print_fs(){
@@ -79,9 +93,85 @@ void print_fs(){
     }
 }
 
+int find_empty_inode(){
+    for(int i = 0 ; i < super.inodes_amount ; i++){
+        if(inode_arr[i].first_block == -1)
+            return i;
+    }
+    return -1;  // on failure
+}
+
+int find_empty_block(){
+    for(int i = 0 ; i < super.blocks_amount ; i++){
+        if(block_arr[i].next_block == -1)
+            return i;
+    }
+    return -1;  // on failure
+}
+
+int allocate_file(int n, const char *target){
+    if(strlen(target) >= ID){
+        printf("given target file name is longer then 8\n");
+        return NULL;
+    }
+    int Inode = find_empty_inode();
+    int Block = find_empty_block();
+    if((Inode == -1) || (Block == -1)){
+        printf("there is no empty inodes or empty blocks\n");
+        return NULL;
+    }
+
+    inode_arr[Inode].first_block = Block;
+    inode_arr[Inode].size = n;
+    strcpy(inode_arr[Inode].id, target);
+    return Inode;
+}
+
+void make_dir(){
+    directory *new_dir = (directory *)malloc(sizeof(directory));
+    int check = allocate_file(sizeof(directory), "new_dir");
+    if(check == 0){
+        inode_arr[check].exist = 'Y';
+        new_dir->amount = 0;
+        // **** maybe need to update the name ****
+        free(new_dir);
+    }
+
+    else{
+        printf("can't create new directory\n");
+        return;
+    }
+}
+
+// need to complete
+ssize_t mywrite(int myfd, const void *buf, size_t count){
+    char *res = (char *)buf;
+    if(inode_arr[myfd].exist == 'Y' || opened[myfd].fd != myfd){
+        printf("error occures\n");
+        return NULL;
+    }
+}
+
+struct mydirent *myreaddir(int target){
+    if(inode_arr[target].exist != 'Y'){
+        printf("error occures\n");
+        return NULL;
+    }
+    directory *dir = (directory *)block_arr[inode_arr[target].first_block].data;
+    return dir;
+}
+
+int mylseek(int myfd, int offset, int whence){
+    if((offset == 0) || (opened[myfd].fd != myfd)){
+        printf("wrong usage of mysleek func, enter positive or negative offset\n");
+        return NULL;
+    }
+    // offset != 0
+    opened[myfd].index = opened[myfd].index + offset;
+    return opened[myfd].index;
+}
+
 int main(){
-    create_fs();
-    sync_fs();
-    print_fs();
+    mymkfs(10);
     printf("done\n");
 }
--- ufs.hpp
+++ ufs.hpp
@@ -4,6 +4,9 @@
 
 #define BLOCK 512
 #define ID 8
+#define MAX_FILES 10000
+#define NAME 10
+#define PATH 50
 
 // credits to : https://www.youtube.com/watch?v=n2AAhiujAqs
 
@@ -13,39 +16,54 @@
  * number of disk blocks
  * size of the disk blocks
  */
-struct superblock{
+typedef struct superblock{
     int blokcs_size;
     int inodes_amount;
     int blocks_amount;
-};
+}s_block;
 
 /**
  * @brief Place to store data
  */
-struct block{
+typedef struct block{
     int next_block;
     char data[BLOCK];
-};
+}block;
 
 /**
  * @brief Reference to a file
  * contains the OS info on a file
+ * exist = Y if pointer to directory
+ * exist = N if pointer to file
  */
-struct inode{
+typedef struct inode{
     int size;
     int first_block;
     char id[ID];
+    char exist;
+}i_node;
+
+typedef struct mydirent{
+    char name[NAME];
+    int fd_num[20];
+    int amount;
+}directory;
+
+struct myopenfile{
+    int index;
+    int fd;
 };
 
 /**
  * @brief write the file system
  */
-void sync_fs();
+void sync_fs(const char *);
 
 /**
- * @brief load a file system
+ * @brief read the file system
+ * 
  */
-void mount_fs();
+void mount_fs(const char *);
 
 /**
  * @brief initialize new filesystem
@@ -53,6 +71,83 @@ void mount_fs();
 void create_fs();
 
 /**
- * @brief print out info about the filesystem
+ * @brief finds an empty inode
+ * return -1 if could not find
+ */
+int find_empty_inode();
+
+/**
+ * @brief finds an empty block
+ * return -1 if could not find
+ */
+int find_empty_block();
+
+/**
+ * @brief finds empty inode
+ * calim it
+ * find / claim a block
+ * return the fd
+ */
+int allocate_file(int, const char *);
+
+/**
+ * @brief print out info about the filesystem from memory
  */
 void print_fs();
+
+/**
+ * @brief create new file with given size
+ */
+void mymkfs(int);
+
+/**
+ * @brief reading/writing UFS
+ * on given source it will read it
+ * on given target it will write to it
+ * 0 -> sucsses
+ * -1 -> failure
+ */
+int mymount(const char *, const char *, const char *, unsigned long, const void *);
+
+/**
+ * @brief 
+ * opening file from given path
+ */
+int myopen(const char *, int);
+
+/**
+ * @brief removing the given fd from myopenfile
+ */
+int myclose(int);
+
+/**
+ * @brief reads data from given fd
+ */
+ssize_t myread(int, void *, size_t);
+
+/**
+ * @brief writes data to given fd
+ */
+ssize_t mywrite(int, const void *, size_t);
+
+/**
+ * @brief increment file ptr by given offset
+ * need to return new pointer
+ */
+int mylseek(int, int, int);
+
+/**
+ * @brief  finds the fd of given dir if exist
+ * if not -> will create new one
+ */
+int myopendir(const char *);
+
+/**
+ * @brief finding the needed directory
+ */
+struct mydirent *myreaddir(int);
+
+/**
+ * @brief creating new directory at first Inode
+ */
+void make_dir();
